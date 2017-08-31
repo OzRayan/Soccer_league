faef9a69f0da133c0e70c913506f1e00724f2397
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2017-08-31 10:22:34 +0100

    Update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -20,6 +20,10 @@ coach = "Dear {},\n\n{}, Sharks,\nPractice at: 10:00  15/08/2017"
 g_name = 'Guardian Name(s)'
 skill = 'Soccer Experience'
 
+def write_text(row, rows, coach)
+    with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
+        textfile.write(coach.format(row[g_name], row['Name']))
+    return textfile
 
 if __name__ == "__main__":
 
@@ -32,41 +36,41 @@ if __name__ == "__main__":
         for row in rows:
             if row[skill] == 'YES' and len(team_Sharks1) < 3:
                 team_Sharks1.append(row['Name']+', '+row[skill]+', '+row[g_name])
-
+                write_text(row, rows, coach)
                 # Crate a txt file for each player guardians
-                with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row[g_name], row['Name']))
+#                 with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row[g_name], row['Name']))
 
             elif row[skill] == 'YES' and len(team_Dragons1) < 3:
                 team_Dragons1.append(row['Name']+', '+row[skill]+', '+row[g_name])
-
-                with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row[g_name], row['Name']))
+                write_text(row, rows, coach)
+#                 with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row[g_name], row['Name']))
 
             elif row[skill] == 'YES' and len(team_Raptors1) < 3:
                 team_Raptors1.append(row['Name']+', '+row[skill]+', '+row[g_name])
-
-                with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row[g_name], row['Name']))
+                write_text(row, rows, coach)
+#                 with open('{}.txt'.format('_'.join(row['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row[g_name], row['Name']))
 
         for row2 in rows:
             if row2[skill] == 'NO' and len(team_Sharks2) < 3:
                 team_Sharks2.append(row2['Name']+', '+row2[skill]+', '+row2[g_name])
-
-                with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row2[g_name], row2['Name']))
+                write_text(row, rows, coach)
+#                 with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row2[g_name], row2['Name']))
 
             elif row2[skill] == 'NO' and len(team_Dragons2) < 3:
                 team_Dragons2.append(row2['Name']+', '+row2[skill]+', '+row2[g_name])
-
-                with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row2[g_name], row2['Name']))
+                write_text(row, rows, coach)
+#                 with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row2[g_name], row2['Name']))
 
             elif row2[skill] == 'NO' and len(team_Raptors2) < 3:
                 team_Raptors2.append(row2['Name']+', '+row2[skill]+', '+row2[g_name])
-
-                with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
-                    textfile.write(coach.format(row2[g_name], row2['Name']))
+                write_text(row, rows, coach)
+#                 with open('{}.txt'.format('_'.join(row2['Name'].lower().split(' '))), 'w') as textfile:
+#                     textfile.write(coach.format(row2[g_name], row2['Name']))
 
         Sharks = team_Sharks1 + team_Sharks2
         Dragons = team_Dragons1 + team_Dragons2
